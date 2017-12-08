c081c1ab7d444cca69b3a0dc3ea78a01e5ecf3dd
Swetha Muralidharan <swethamuralidharan@swethas-mbp.attlocal.net>
2017-12-07 22:04:44 -0800

    giphy search done

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,55 @@
-$(document).on("ready", function(){
+
+$(document).on("ready", function()
+{
+  var url_link="http://api.giphy.com/v1/gifs/search";
+  console.log("fully loaded");
+  $.ajax(
+  {
+    method: 'GET',
+    url: url_link,
+    dataType: 'json',
+    success: onSuccess
+
+  })
+$(".btn-primary").on("click",function(e)
+{
+  e.preventDefault();
+  $.ajax({
+    method: 'GET',
+    url: url_link,
+    dataType: 'json',
+    data:$("form").serialize(),
+    success: onSuccess
+  })
+})
+
+$(".btn-secondary").on("click",function(e)
+{
+  e.preventDefault();
+  $.ajax({
+
+    method: 'GET',
+    url: url_link,
+    dataType: 'json',
+    data:$("form").serialize(),
+    offset:25,
+    success: onSuccess,
+    error:onError
+
+  })
+})
+
+function onError(json_obj)
+{
+  console.log("error");
+}
+function onSuccess(obj)
+{
+    console.log(obj);
+    $.each(obj.data,function(k,v)
+    {
+      $('.gif-gallery').append(`<img src="${obj.data[k].images.fixed_width.url}" >`);
+    })
+};
 
 });
